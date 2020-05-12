7d1a5d4e4d6213bb37edf6963684fe50df9ea48a
meijieru <meijieru@gmail.com>
2020-05-12 16:49:33 +0800

    [zsh] p10k

--- dotfiles/profile.d/exports.sh
+++ dotfiles/profile.d/exports.sh
@@ -36,3 +36,13 @@ export GTAGSLABEL='native-pygments'
 export GTAGSCONF=$HOME/.config/gtags.conf
 
 export XAUTHORITY=$HOME/.Xauthority
+
+# XDG
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_DATA_HOME=$HOME/.local/share
+
+# export ZDOTDIR=$XDG_CONFIG_HOME/zsh
+export IPYTHONDIR=$XDG_CONFIG_HOME/ipython
+export JUPYTER_CONFIG_DIR=$XDG_CONFIG_HOME/jupyter
+# export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/config
